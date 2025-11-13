7a26ac990ec5e029d8d41287c7b857d3ba88e4cf
dewydd <117913498+Dewyydd@users.noreply.github.com>
2025-11-13 11:56:31 +0100

    big elorehaladas

--- adatszures/adatszures/Program.cs
+++ adatszures/adatszures/Program.cs
@@ -170,12 +170,163 @@ namespace adatszures
             }
             Console.Clear();
 
-            Bekeres(ValasztottOpciok, SGenre, MGenre);
+            List<string> SzurtAdatok = Bekeres(ValasztottOpciok, SGenre, MGenre);
+
+            foreach (var a in SzurtAdatok) 
+            {
+                Console.WriteLine(a);
+            }
         }
 
-        static void Bekeres(List<bool> KivalasztottKategoriak, List<string> SGenre, List<string> MGenre) 
+        static List<string> Bekeres(List<bool> ValasztottOpciok, List<string> SGenre, List<string> MGenre) 
         {
-            Console.WriteLine("Szia");
+            List<string> SzuresAdatok = new List<string>();
+
+            if (ValasztottOpciok[0] == true) 
+            {
+                SzuresAdatok.Add(Ertekeles());
+                Console.Clear();
+            }
+
+            if (ValasztottOpciok[1] == true)
+            {
+                SzuresAdatok.Add(Tipus(false));
+
+                if (ValasztottOpciok[3] == true)
+                {
+                    SzuresAdatok.Add(Tipus(true));
+                }
+
+                Console.Clear();
+            }
+
+            if (ValasztottOpciok[2] == true)
+            {
+                SzuresAdatok.Add(Mufaj());
+                Console.Clear();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sikeres adat bekérés.\nNyomj meg egy gombot a folytatáshoz!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+
+            return SzuresAdatok;
+        }
+
+        static string Ertekeles() 
+        {
+            bool helyes = false;
+            double ertekeles = 0.0;
+
+            while (helyes != true) 
+            {            
+                Console.WriteLine("Mi legyen a minimum értékelés? [pl. 5.5/5,5]");
+                try
+                {
+                    Console.Write("Értékelés: ");
+                    ertekeles = double.Parse(Console.ReadLine().Trim().Replace(".",","));
+
+                    if (ertekeles < 0.0 || ertekeles > 10.0)
+                    {
+                        Console.WriteLine("Az értékelésnem [0.0] és [10.0] között kell lennie.");
+                    }
+
+                    else 
+                    {
+                        helyes = true;
+                    } 
+                }
+                catch 
+                {
+                    Console.WriteLine("Nem megfelelő adat.");
+                }
+            }
+
+            return "E" + Convert.ToString(ertekeles);
+        }
+
+        static string Tipus(bool kellHossz)
+        {
+            string[] Tipusok = { "Dokumentum Film", "Film", "Sorozat" };
+            int sorszam = 0;
+            bool helyes = false;
+
+            Console.WriteLine("Milyen típsú műsort akarsz nézni? A következők érhetőek el:");
+            for (int i = 0; i < Tipusok.Length; i++) 
+            {
+                Console.WriteLine($"  {i+1}. {Tipusok[i]}");
+            }
+
+
+            while (helyes != true) 
+            {
+                try
+                {
+                    Console.Write("Sorszám: ");
+                    sorszam = int.Parse(Console.ReadLine().Trim());
+
+                    if ((sorszam - 1) < 0 || (sorszam - 1) > Tipusok.Length)
+                    {
+                        Console.WriteLine("Csak a listában szereplő sorszámot adhatsz meg.");
+                    }
+
+                    else 
+                    {
+                        helyes = true;
+                    }
+                }
+
+                catch 
+                {
+                    Console.WriteLine("Nem megfelelő adat.");
+                }
+
+            }
+
+            string output = "";
+
+            if (sorszam - 1 == 0) 
+            {
+                output = "documentary";
+            }
+
+            if (sorszam - 1 == 1)
+            {
+                output = "movie";
+            }
+
+            if (sorszam - 1 == 2)
+            {
+                output = "series";
+            }
+
+            if (kellHossz) 
+            {
+                Hossz(output);
+            }
+
+            return "T" + output;
+        }
+
+        static string Mufaj()
+        {
+            return "X";
+        }
+
+        static string Hossz(string Tipus)
+        {
+            if (Tipus == "movie" || Tipus == "documentary")
+            {
+
+            }
+
+            else 
+            {
+
+            }
+
+            return "X";
         }
     }
 }
